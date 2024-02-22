cd6e715c8537a2748b858bdb0084f182a3d499cb
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-22 16:22:33 +0100

    Initial Commit

---
+++ Give me five/Give_me_fiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Give_me_fiveApp.swift
+//  Give me five
+//
+//  Created by  Ixart on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Give_me_fiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
