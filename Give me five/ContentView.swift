9f472b22d5410927dd8e75d7bafa595b24e1499f
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-23 18:39:08 +0100

    il  manque le rebont du toast

--- Give me five/ContentView.swift
+++ Give me five/ContentView.swift
@@ -6,47 +6,56 @@
 //
 
 import SwiftUI
+import SimpleToast
 
 struct ContentView: View {
-    @State private var isShowingDialog = false
+    @State var showToast: Bool = false
+    
+    private let toastOptions = SimpleToastOptions(
+           hideAfter: 10
+       )
 
     var body: some View {
-        
-        
-        
-        VStack {
+        VStack{
             
+            Button(action: {withAnimation {showToast.toggle()}}, label: {
+                                   Text("give me five")
+                                       .font(.system(size: 40))
+                                       .bold()
+                
+                                   Image(systemName: "hand.raised.fill")
+                                       .font(.system(size: 40))
+                                       .foregroundColor(.purple)
+                
+                               })
             
-            
-            
-        
-            
-            Button(action: {}, label: {
-
+            .simpleToast(isPresented: $showToast, options: toastOptions) {
+                
+                
+                
+                Text("hey coucou toi")
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .shadow(color: Color.black.opacity(0.5), radius: 3, x: 0, y: 2)
 
                 
-                Text("give me five")
-                    .font(.system(size: 40))
-                    .bold()
-                Image(systemName: "hand.raised.fill")
-                    .font(.system(size: 40))
-                    .foregroundColor(.purple)
-                    
+                .frame(width: 230, height: 200)
+                .padding()
+                .background(Color.cyan.opacity(0.9))
+                .foregroundColor(Color.black)
+                .font(.title)
+                .cornerRadius(20)
+                .padding(.top)
                 
-
-
-            })
-            
-            
-            
-            
-               
                 
-            
-           
+                
+                Text("😁")
+                    .shadow(color: Color.black.opacity(0.5), radius: 3, x: 3, y: 8)
+                    .font(.system(size: 30))
+                    .padding(.bottom, 90) // Ajoute un espace en bas de l'image
 
-
-
+                
+            }
             
             
             
@@ -55,13 +64,23 @@ struct ContentView: View {
             
             
             
-            
-            
-            
-        } // fin vstack
-    } //fin body
-} // STRUCT
+        } // FIN VStACK
+        
+        
+        
+        
+        
+        
+        
+       
+        
+        
+        }//fin body
+    } // stuct
 
 #Preview {
     ContentView()
 }
+
+
+
