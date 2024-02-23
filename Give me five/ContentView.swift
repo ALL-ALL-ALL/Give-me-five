01fe3aad700f19f1cc2c12294629ec8e7f94261f
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-23 02:37:58 +0100

    il manque le genre de pop up

--- Give me five/ContentView.swift
+++ Give me five/ContentView.swift
@@ -8,16 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingDialog = false
+
     var body: some View {
+        
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            
+            
+            
+            
+        
+            
+            Button(action: {}, label: {
+
+
+                
+                Text("give me five")
+                    .font(.system(size: 40))
+                    .bold()
+                Image(systemName: "hand.raised.fill")
+                    .font(.system(size: 40))
+                    .foregroundColor(.purple)
+                    
+                
+
+
+            })
+            
+            
+            
+            
+               
+                
+            
+           
+
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        } // fin vstack
+    } //fin body
+} // STRUCT
 
 #Preview {
     ContentView()
